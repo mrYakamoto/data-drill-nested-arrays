36555db53480a2c2672032b464f977c30a101445
Torey Hickman <torey@toreyhickman.com>
2015-09-24 10:40:38 -0500

    Simplify chessboard specs

--- spec/chessboard_spec.rb
+++ spec/chessboard_spec.rb
@@ -16,32 +16,24 @@ describe 'generate_chessboard' do
   end
 
   describe 'piece placement' do
-    let(:black_back_row) { ['B Rook', 'B Knight', 'B Bishop', 'B Queen', 'B King', 'B Bishop', 'B Knight', 'B Rook'] }
-    let(:white_back_row) { ['W Rook', 'W Knight', 'W Bishop', 'W Queen', 'W King', 'W Bishop', 'W Knight', 'W Rook'] }
-
     it 'has a first row of black backline pieces' do
-      expect(board.first).to eq black_back_row
+      expect(board.first).to eq ['B Rook', 'B Knight', 'B Bishop', 'B Queen', 'B King', 'B Bishop', 'B Knight', 'B Rook']
     end
 
     it 'has a last row of while backline pieces' do
-      expect(board.last).to eq white_back_row
+      expect(board.last).to eq ['W Rook', 'W Knight', 'W Bishop', 'W Queen', 'W King', 'W Bishop', 'W Knight', 'W Rook']
     end
 
     it 'has a second row of all black pawns' do
-      second_row = board[1]
-
-      expect(second_row.all? { |piece| piece == 'B Pawn' }).to be true
+      expect(board[1]).to eq Array.new(8) { 'B Pawn' }
     end
 
     it 'has a second to last row of all white pawns' do
-      second_to_last_row = board[-2]
-
-      expect(second_to_last_row.all? { |piece| piece == 'W Pawn' }).to be true
+      expect(board[-2]).to eq Array.new(8) { 'W Pawn' }
     end
 
     it 'has four empty rows' do
       empty_rows_count = board.count { |row| row.all?(&:nil?) }
-
       expect(empty_rows_count).to eq 4
     end
   end
